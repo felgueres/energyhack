f910dd72811a284136c76678da5daa8b94af9d46
felgueres <felgueres@catacaldo.local>
2017-04-06 21:45:57 -0700

    erasing verbosity

--- src/viz.py
+++ src/viz.py
@@ -16,28 +16,6 @@ import itertools
 data = pd.read_pickle('myanmar_data.pickle')
 data.dropna(inplace = True)
 
-#FORMATTING
-# movies["color"] = np.where(movies["Oscars"] > 0, "orange", "grey")
-# movies["alpha"] = np.where(movies["Oscars"] > 0, 0.9, 0.25)
-# movies.fillna(0, inplace=True)  # just replace missing values with zero
-# movies["revenue"] = movies.BoxOffice.apply(lambda x: '{:,d}'.format(int(x)))
-
-#DONT KNOW WHAT THIS IS.
-# with open(join(dirname(__file__), "razzies-clean.csv")) as f:
-#     razzies = f.read().splitlines()
-# movies.loc[movies.imdbID.isin(razzies), "color"] = "purple"
-# movies.loc[movies.imdbID.isin(razzies), "alpha"] = 0.9
-
-
-# axis_map = {
-#     "Tomato Meter": "Meter",
-#     "Numeric Rating": "numericRating",
-#     "Number of Reviews": "Reviews",
-#     "Box Office (dollars)": "BoxOffice",
-#     "Length (minutes)": "Runtime",
-#     "Year": "Year",
-# }
-
 axis_map = {
     "Market Size in kW": "underserved_mkt_size_kW",
     "Market Size in USD": "underserved_mkt_size_USD",
@@ -59,30 +37,8 @@ gov_revenue = Slider(title="Government Revenue (K)", start=0, end=175000, value=
 access_to_comm = Slider(title="Communication Access (Phones/Household)", start=0, end=1, value=data.mobile_phone_per_HH.mean(), step=0.01)
 Access_by_car = Slider(title="Communication Access (Cars/Household)", start = 0, end=0.36, value=data.car_access_per_HH.mean(), step=0.0001 )
 
-# reviews = Slider(title="Minimum number of reviews", value=80, start=10, end=300, step=10)
-# min_year = Slider(title="Year released", start=1940, end=2014, value=1970, step=1)
-# max_year = Slider(title="End Year released", start=1940, end=2014, value=2014, step=1)
-# oscars = Slider(title="Minimum number of Oscar wins", start=0, end=4, value=0, step=1)
-# boxoffice = Slider(title="Dollars at Box Office (millions)", start=0, end=800, value=0, step=1)
-
-#This could be used for filtering
-# genre = Select(title="Genre", value="All",
-#                options=open(join(dirname(__file__), 'genres.txt')).read().split())
-
-# director = TextInput(title="Director name contains")
-# cast = TextInput(title="Cast names contains")
-
 x_axis = Select(title="X Axis", options=sorted(axis_map.keys()), value="Market Size in kW")
 y_axis = Select(title="Y Axis", options=sorted(axis_map.keys()), value="Income per Capita (yr)")
-
-# Create Column Data Source that will be used by the plot
-# source = ColumnDataSource(data=dict(x=[],
-#                                     y=[],
-#                                     color=[],
-#                                     title=[],
-#                                     year=[],
-#                                     revenue=[],
-#                                     alpha=[]))
 
 source = ColumnDataSource(data=dict(x=[],
                                     y=[],
@@ -103,7 +59,6 @@ hover = HoverTool(tooltips=[
 ])
 
 p = figure(plot_height=600, plot_width=700, title="", toolbar_location=None, tools=[hover])
-# p.circle(x="x", y="y", source=source, size=7, line_color=None)
 
 p.circle(x="x", y="y", source=source, size='blob_size', color="color", line_color=None, fill_alpha=0.5)
 
@@ -118,25 +73,6 @@ def select():
                        (data.car_access_per_HH >= Access_by_car.value)].copy()
 
     return selected
-
-# def select_movies():
-#     genre_val = genre.value
-#     director_val = director.value.strip()
-#     cast_val = cast.value.strip()
-#     selected = movies[
-#         (movies.Reviews >= reviews.value) &
-#         (movies.BoxOffice >= (boxoffice.value * 1e6)) &
-#         (movies.Year >= min_year.value) &
-#         (movies.Year <= max_year.value) &
-#         (movies.Oscars >= oscars.value)
-#     ]
-#     if (genre_val != "All"):
-#         selected = selected[selected.Genre.str.contains(genre_val)==True]
-#     if (director_val != ""):
-#         selected = selected[selected.Director.str.contains(director_val)==True]
-#     if (cast_val != ""):
-#         selected = selected[selected.Cast.str.contains(cast_val)==True]
-#     return selected
 
 def update():
 
@@ -160,27 +96,7 @@ def update():
         color = df['color'],
         blob_size = df['blob_size'])
 
-# def update():
-#     df = select_movies()
-#     x_name = axis_map[x_axis.value]
-#     y_name = axis_map[y_axis.value]
-#
-#     p.xaxis.axis_label = x_axis.value
-#     p.yaxis.axis_label = y_axis.value
-#     p.title.text = "%d movies selected" % len(df)
-#     source.data = dict(
-#         x=df[x_name],
-#         y=df[y_name],
-#         color=df["color"],
-#         title=df["Title"],
-#         year=df["Year"],
-#         revenue=df["revenue"],
-#         alpha=df["alpha"],
-#     )
-
 controls = [min_market_size_kW, min_market_size_USD, min_income_per_capita, gov_revenue, access_to_comm,Access_by_car, x_axis, y_axis]
-
-# controls = [reviews, boxoffice, genre, min_year, max_year, oscars, director, cast, x_axis, y_axis]
 
 for control in controls:
     control.on_change('value', lambda attr, old, new: update())
